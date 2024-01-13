e6f6b254520644cc08542e4bf395180ca40598f2
Anup Rai <raileohang@gmail.com>
2024-01-13 19:03:12 +0545

    refactor: setup and implement react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import AboutPage from './pages/AboutPage.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<AboutPage />} />
+    </>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <div className="App">
+      <main className="main_container">
+        <h1>ABOUT PAGE</h1>
+        <p>This is the about page</p>
+      </main>
+    </div>
+  );
+}
